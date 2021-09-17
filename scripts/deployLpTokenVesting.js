269b524008f6a989fa9eaec64a0b05a66c3b9ad7
Frederic Gingras <frederic@gingras.cc>
2021-09-17 15:41:46 -0400

    prepare to deploy BNPL LpTokenVesting contract

--- scripts/deployLpTokenVesting.js
+++ scripts/deployLpTokenVesting.js
@@ -1,22 +1,19 @@
 const hre = require("hardhat");
 
 // const xruneContract = "0x0fe3ecd525d16fa09aa1ff177014de5304c835e2"; // ropsten
-const xruneContract = "0x69fa0fee221ad11012bab0fdb45d444d3d2ce71c"; // mainnet
 // const sushiRouter = "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506"; // ropsten
+const xruneContract = "0x69fa0fee221ad11012bab0fdb45d444d3d2ce71c"; // mainnet
 const sushiRouter = "0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F"; // mainnet
 
 async function main() {
   const Contract = await hre.ethers.getContractFactory("LpTokenVesting");
   const args = [
     xruneContract, // token
-    "0x2610F0bFC21EF389fe4D03CFB7De9ac1E6C99D6E", // offering token
+    "0x84d821F7FbDD595c4C4A50842913e6b1E07d7a53", // offering token
     sushiRouter, // sushi router
     0, // vesting cliff
     63072000, // vesting length (2 year)
-    [
-      "0x69539C1c678dFd26E626f109149b7cEBDd5E4768",
-      "0x6d2Cb3ECE7e6A280a1866E722e66063020533778"
-    ] // parties
+    ["0x69539C1c678dFd26E626f109149b7cEBDd5E4768", ""] // parties
   ];
   const contract = await Contract.deploy(...args, {
     //gasLimit: 5000000,
